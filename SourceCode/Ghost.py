33681d02296bf39f28749781bdb823e519d95460
Juan Pablo Carrera <juanpablo.carrera.m@gmail.com>
2026-04-25 00:15:48 -0600

    Feat: Add movimiento_a_estrella logic

--- SourceCode/Ghost.py
+++ SourceCode/Ghost.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import heapq
 from Entidad import Entidad
 mapa_numeros = {
         10: [[1, 0], [0, 1]],
@@ -12,18 +13,74 @@ mapa_numeros = {
         24: [[1, 0], [0,-1], [0, 1]],
         25: [[1, 0], [-1,0], [0,-1], [0, 1]],
         26: [[1, 0]],
-        27: [[-1,0]]
+        27: [[-1,0]],
+        28: [[1,0],[-1,0]],
+        29:[[0,1],[0,-1]]
         }   
 xMC = [22,50,92,136,178,221,262,308,350,378]
 yMC = [20,72,109,150,187,225,264,303,342,382]
 class Ghost(Entidad):
     def __init__(self,mapa, mc, x_px_to_mc, y_px_to_mc,x_inicial, y_inicial,x_mc, y_mc, pacman):
-        super().__init__(mapa, mc, x_px_to_mc, y_px_to_mc,x_inicial, y_inicial)
-        self.xMC = x_mc
-        self.yMC = y_mc
+        super().__init__(mapa, mc, x_px_to_mc, y_px_to_mc,x_inicial, y_inicial, x_mc, y_mc)
         self.angulo_direccion = 0
         self.pacman = pacman
 
+    def calcular_esquinas_vecinas(self,esquina):
+        esquinas_vecinas = []
+        indice_x = self.XPxToMC[esquina[0]]
+        indice_y = self.YPxToMC[esquina[1]]
+        if(indice_x != -1 and indice_y != -1):
+            movimientos = mapa_numeros.get(self.MC[indice_y][indice_x])
+        else:
+            return esquinas_vecinas
+        if movimientos is not None:
+            for movimiento in movimientos:
+                esquinas_vecinas.append((self.xMC[indice_x + movimiento[0]], self.yMC[indice_y + movimiento[1]]))
+        return esquinas_vecinas
+    
+    def calcular_manhattan(self,esquina1,esquina2):
+        return abs(esquina1[0] - esquina2[0]) + abs(esquina1[1] - esquina2[1])
+    
+    """ Algoritmo de A* en mapa Pacman con esquinas como nodos"""
+    def a_estrella(self,nodoOrigen,nodoObjetivo):
+        """ Se verifica que el nodo origen y el nodo objetivo sean validos (que se encuentren en una esquina) """
+        if (self.XPxToMC[nodoOrigen[0]] != -1 and self.YPxToMC[nodoOrigen[1]] != -1) and (self.XPxToMC[nodoObjetivo[0]] != -1 and self.YPxToMC[nodoObjetivo[1]] != -1):
+            open_list = []
+            closed_list = set()
+            caracteristicas_nodos = {}
+            manhattan_primero = self.calcular_manhattan((nodoOrigen[0],nodoOrigen[1]), nodoObjetivo)
+            heapq.heappush(open_list,(manhattan_primero, nodoOrigen[0], nodoOrigen[1]))
+            caracteristicas_nodos[(nodoOrigen[0],nodoOrigen[1])] = {"g":0, "h": manhattan_primero, "f": manhattan_primero, "padre":None}
+            while open_list:
+                nodo_actual = heapq.heappop(open_list)
+                """ Si el nodo actual tiene un valor de f mayor al valor de f almacenado significa que ya se encontro un camino mas corto hacia ese nodo y se ignora """
+                if caracteristicas_nodos[(nodo_actual[1], nodo_actual[2])]["f"] < nodo_actual[0]:
+                    continue
+                """ Si el nodo actual es el objetivo se reconstruye el camino y se actualiza el movimiento del fantasma """
+                if (nodo_actual[1], nodo_actual[2]) == nodoObjetivo:
+                    nodo_actual = (nodo_actual[1], nodo_actual[2])
+                    camino = []
+                    while nodo_actual is not None:
+                        camino.append((nodo_actual[0], nodo_actual[1]))
+                        nodo_actual = caracteristicas_nodos[(nodo_actual[0], nodo_actual[1])]["padre"]
+                    camino.reverse()
+                    return camino
+                """ Se agrega el nodo con menor f a la lista cerrada """
+                closed_list.add((nodo_actual[1], nodo_actual[2]))
+                esquinas_vecinas = self.calcular_esquinas_vecinas((nodo_actual[1], nodo_actual[2]))
+                """ Se calculan las caracteristicas de los nodos vecinos del nodo con menor f"""
+                for esquina_vecina in esquinas_vecinas:
+                    """ Si el nodo vecino ya fue evaluado se ignora """
+                    if esquina_vecina in closed_list:
+                        continue
+                    g = caracteristicas_nodos[(nodo_actual[1], nodo_actual[2])]["g"] + 1
+                    h = self.calcular_manhattan(esquina_vecina, nodoObjetivo)
+                    f = g + h
+                    """ Si no esta en la lista abierta o se encuentra un camino mas corto se actualizan las caractersticas del nodo """
+                    if esquina_vecina not in caracteristicas_nodos or g < caracteristicas_nodos[esquina_vecina]["g"]:
+                        caracteristicas_nodos[esquina_vecina] = {"g": g, "h": h, "f": f, "padre": (nodo_actual[1], nodo_actual[2])}
+                        heapq.heappush(open_list, (f, esquina_vecina[0], esquina_vecina[1]))
+                        
     def movimiento_fantasma_aleatorio(self):
         actual = self.esquinaActual()
         listamovimiento = mapa_numeros.get(actual)
@@ -54,21 +111,27 @@ class Ghost(Entidad):
             elif self.y > esquinas_posibles[indice_esquina_mas_cercana][1]:
                 movimiento[1] = -1
             self.update(movimiento)
-            
-    def calcular_esquinas_vecinas(self,esquina):
-        esquinas_vecinas = []
-        indice_x = self.XPxToMC[esquina[0]]
-        indice_y = self.YPxToMC[esquina[1]]
-        if(indice_x != -1 and indice_y != -1):
-            movimientos = mapa_numeros.get(self.MC[indice_y][indice_x])
-        else:
-            return esquinas_vecinas
-        if movimientos is not None:
-            for movimiento in movimientos:
-                esquinas_vecinas.append((self.xMC[indice_x + movimiento[0]], self.yMC[indice_y + movimiento[1]]))
-        return esquinas_vecinas
-    """ Metodo a implementar para movimiento cooperativo entre fantasmas  """
+
+    """ Movimiento del fantasma utilizando el algoritmo de A* """
     def movimiento_fantasma_a_estrella(self,nodoObjetivo):
-        pass
+        if self.esquinaActual() != -1:
+            if self.x == nodoObjetivo[0] and self.y == nodoObjetivo[1]:
+                self.movimiento_fantasma_heuristico()
+                return
+            camino = self.a_estrella((self.x,self.y), nodoObjetivo)
+            if camino is not None and len(camino) > 1:
+                siguiente_esquina = camino[1]
+                movimiento = [0,0]
+                if self.x < siguiente_esquina[0]:
+                    movimiento[0] = 1
+                elif self.x > siguiente_esquina[0]:
+                    movimiento[0] = -1
+                if self.y < siguiente_esquina[1]:
+                    movimiento[1] = 1
+                elif self.y > siguiente_esquina[1]:
+                    movimiento[1] = -1
+                self.update(movimiento)
+            else:
+                self.movimiento_fantasma_heuristico()
     def update(self, dir):
         self.direccion = [dir[0],dir[1]]
--- SourceCode/Pacman.py
+++ SourceCode/Pacman.py
@@ -2,32 +2,48 @@ from Entidad import Entidad
 import numpy as np
 import math
 class Pacman(Entidad):
-    def __init__(self,mapa, mc, x_mc, y_mc,x_inicial, y_inicial):
-        super().__init__(mapa, mc, x_mc, y_mc,x_inicial, y_inicial)
+    def __init__(self,mapa, mc, x_pxmc, y_pxmc,x_inicial, y_inicial,x_mc,y_mc):
+        super().__init__(mapa, mc, x_pxmc, y_pxmc,x_inicial, y_inicial,x_mc,y_mc)
+        self.esquina_futura = (136,303)
+        self.esquina_reciente = (178,303)
+        self.esquina_respaldo = (178,303)
     def performObjectCollisionLogic(self):
         esquinaActual = self.esquinaActual()
-        if esquinaActual != -1 and esquinaActual != 0:
+        if esquinaActual != -1:
+            self.esquina_respaldo = self.esquina_reciente
+            self.esquina_reciente = (self.x,self.y)
             if self.direccionFutura == [1,0] and (esquinaActual == 10 or esquinaActual == 12 or esquinaActual == 21 or esquinaActual == 23 or esquinaActual == 24 or esquinaActual == 25 or esquinaActual == 26 or esquinaActual == 28):
                 self.update(self.direccionFutura)
+                self.esquina_futura  = (self.xMC[self.XPxToMC[self.x] + self.direccionFutura[0]], self.yMC[self.YPxToMC[self.y] + self.direccionFutura[1]])
             elif self.direccionFutura == [-1,0] and (esquinaActual == 11 or esquinaActual == 13 or esquinaActual == 21 or esquinaActual ==22 or esquinaActual == 23 or esquinaActual == 25 or esquinaActual == 27 or esquinaActual == 28):
                 self.update(self.direccionFutura)
-            elif self.direccionFutura == [0,1] and (esquinaActual == 10 or esquinaActual == 11 or esquinaActual == 21 or esquinaActual == 22 or esquinaActual == 24 or esquinaActual == 25):
+                self.esquina_futura  = (self.xMC[self.XPxToMC[self.x] + self.direccionFutura[0]], self.yMC[self.YPxToMC[self.y] + self.direccionFutura[1]])
+            elif self.direccionFutura == [0,1] and (esquinaActual == 10 or esquinaActual == 11 or esquinaActual == 21 or esquinaActual == 22 or esquinaActual == 24 or esquinaActual == 25 or esquinaActual == 29):
                 self.update(self.direccionFutura)
-            elif self.direccionFutura == [0,-1] and (esquinaActual == 12 or esquinaActual == 13 or esquinaActual == 22 or esquinaActual == 23 or esquinaActual == 24 or esquinaActual == 25):
+                self.esquina_futura  = (self.xMC[self.XPxToMC[self.x] + self.direccionFutura[0]], self.yMC[self.YPxToMC[self.y] + self.direccionFutura[1]])
+            elif self.direccionFutura == [0,-1] and (esquinaActual == 12 or esquinaActual == 13 or esquinaActual == 22 or esquinaActual == 23 or esquinaActual == 24 or esquinaActual == 25 or esquinaActual == 29):
                 self.update(self.direccionFutura)
+                self.esquina_futura  = (self.xMC[self.XPxToMC[self.x] + self.direccionFutura[0]], self.yMC[self.YPxToMC[self.y] + self.direccionFutura[1]])
             else:
                 if self.direccion == [1,0] and (esquinaActual == 10 or esquinaActual == 12 or esquinaActual == 21 or esquinaActual == 23 or esquinaActual == 24 or esquinaActual == 25 or esquinaActual == 26 or esquinaActual == 28):
                     self.update(self.direccion)
+                    self.esquina_futura  = (self.xMC[self.XPxToMC[self.x] + self.direccionFutura[0]], self.yMC[self.YPxToMC[self.y] + self.direccionFutura[1]])
                 elif self.direccion == [-1,0] and (esquinaActual == 11 or esquinaActual == 13 or esquinaActual == 21 or esquinaActual ==22 or esquinaActual == 23 or esquinaActual == 25 or esquinaActual == 27 or esquinaActual == 28):
                     self.update(self.direccion)
-                elif self.direccion == [0,1] and (esquinaActual == 10 or esquinaActual == 11 or esquinaActual == 21 or esquinaActual == 22 or esquinaActual == 24 or esquinaActual == 25):
+                    self.esquina_futura  = (self.xMC[self.XPxToMC[self.x] + self.direccionFutura[0]], self.yMC[self.YPxToMC[self.y] + self.direccionFutura[1]])
+                elif self.direccion == [0,1] and (esquinaActual == 10 or esquinaActual == 11 or esquinaActual == 21 or esquinaActual == 22 or esquinaActual == 24 or esquinaActual == 25 or esquinaActual == 29):
                     self.update(self.direccion)
-                elif self.direccion == [0,-1] and (esquinaActual == 12 or esquinaActual == 13 or esquinaActual == 22 or esquinaActual == 23 or esquinaActual == 24 or esquinaActual == 25):
+                    self.esquina_futura  = (self.xMC[self.XPxToMC[self.x] + self.direccionFutura[0]], self.yMC[self.YPxToMC[self.y] + self.direccionFutura[1]])
+                elif self.direccion == [0,-1] and (esquinaActual == 12 or esquinaActual == 13 or esquinaActual == 22 or esquinaActual == 23 or esquinaActual == 24 or esquinaActual == 25 or esquinaActual == 29):
                     self.update(self.direccion)
+                    self.esquina_futura  = (self.xMC[self.XPxToMC[self.x] + self.direccionFutura[0]], self.yMC[self.YPxToMC[self.y] + self.direccionFutura[1]])
                 else:
                     self.update([0,0])
+                    self.esquina_futura = self.esquina_respaldo
     def update(self, dir):
         self.direccion = [dir[0],dir[1]]
         self.angulo_direccion = math.atan2(self.direccion[1], self.direccion[0]) * 180 / math.pi
         if self.angulo_direccion < 0:
             self.angulo_direccion += 360
+
+    
